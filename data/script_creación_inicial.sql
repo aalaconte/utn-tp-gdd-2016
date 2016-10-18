38e63cf1e72854d046b0846fd3e79fb4c5ef2b20
gstella <gstella@users.noreply.github.com>
2016-10-18 15:26:04 -0300

    Update script_creación_inicial.sql
    Correciones tipos en columnas

--- data/script_creación_inicial.sql
+++ data/script_creación_inicial.sql
@@ -8,6 +8,8 @@ GO
 CREATE SCHEMA [pico_y_pala] AUTHORIZATION [gd]
 GO
 
+
+
 create table pico_y_pala.funcionalidad 
 (
 	fun_id int primary key,
@@ -30,7 +32,7 @@ create table pico_y_pala.rol_funcionalidad
 
 create table pico_y_pala.usuario
 (
-	usu_username varchar (50) primary key,
+	usu_username varchar (100) primary key,
 	usu_password varchar (50), --analizar tipo
 	usu_nombre varchar (255),
 	usu_apellido varchar (255),
@@ -47,13 +49,13 @@ create table pico_y_pala.usuario
 create table pico_y_pala.rol_usuario
 (
 	rfu_rol_id int foreign key references pico_y_pala.rol (rol_id),
-	rfu_fun_id varchar (50) foreign key references pico_y_pala.usuario(usu_username),
+	rfu_fun_id varchar (100) foreign key references pico_y_pala.usuario(usu_username),
 	primary key (rfu_rol_id,rfu_fun_id)
 );
 
 create table pico_y_pala.profesional 
 (
-	pro_username varchar(50),
+	pro_username varchar(100),
 	pro_matricula int, --ver como generarlo
 	primary key (pro_username),
 	foreign key (pro_username) references pico_y_pala.usuario(usu_username)
@@ -61,20 +63,20 @@ create table pico_y_pala.profesional
 
 create table pico_y_pala.tipo_especialidad
 (
-	tes_id int primary key,
-	tes_desc varchar(100)
+	tes_id numeric(18,0) primary key,
+	tes_desc varchar(255)
 );
 
 create table pico_y_pala.especialidad
 (
 	esp_id int primary key,
-	esp_desc varchar (100),
-	esp_test_id int foreign key references pico_y_pala.tipo_especialidad(tes_id),
+	esp_desc varchar (255),
+	esp_test_id numeric (18,0) foreign key references pico_y_pala.tipo_especialidad(tes_id),
 );
 
 create table pico_y_pala.profesional_especialidad
 (
-	pes_pro_username  varchar(50) foreign key references pico_y_pala.profesional(pro_username),
+	pes_pro_username  varchar(100) foreign key references pico_y_pala.profesional(pro_username),
 	pes_esp_id int foreign key references pico_y_pala.especialidad (esp_id),
 	primary key (pes_pro_username, pes_esp_id)
 );
@@ -82,7 +84,7 @@ create table pico_y_pala.profesional_especialidad
 create table pico_y_pala.agenda
 (
 	age_id int,
-	age_pro_username varchar (50), 
+	age_pro_username varchar (100), 
 	age_esp_id int, 
 	age_deste datetime,
 	age_hasta datetime,
@@ -98,7 +100,7 @@ create table pico_y_pala.dia
 
 create table pico_y_pala.dia_por_agenda
 (
-	pes_pro_username varchar (50),
+	pes_pro_username varchar (100),
 	pes_age_id int, --agregada
 	pes_esp_id int,
 	pes_dia int foreign key references pico_y_pala.dia (dia_id),
@@ -124,12 +126,12 @@ create table pico_y_pala.planes
 create table pico_y_pala.grupo_familiar
 (
 	gpo_id int primary key,
-	gpo_titular varchar (50)
+	gpo_titular varchar (100) 
 );
 
 create table pico_y_pala.afiliado
 (
-	afi_username varchar (50) primary key,
+	afi_username varchar (100) primary key,
 	afi_nro_afiliado int,
 	afi_eci_id int foreign key references pico_y_pala.estado_civil (eci_id),
 	afi_gpo_id int foreign key references pico_y_pala.grupo_familiar (gpo_id),
@@ -145,9 +147,9 @@ add foreign key (gpo_titular) references pico_y_pala.afiliado (afi_username)
 create table pico_y_pala.audit_cambio_plan
 (
 	acp_id int primary key,
-	acp_afiliado varchar (50) foreign key references pico_y_pala.afiliado (afi_username),
+	acp_afiliado varchar (100) foreign key references pico_y_pala.afiliado (afi_username),
 	acp_fecha datetime,
-	acp_motivo varchar (100),
+	acp_motivo varchar (255),
 	acp_plan_anterior int foreign key references pico_y_pala.planes (pla_id),
 	acp_plan_nuevo int foreign key references pico_y_pala.planes (pla_id)
 );
@@ -156,14 +158,14 @@ create table pico_y_pala.audit_cambio_plan
 create table pico_y_pala.tipo_cancelacion
 (
 	tca_id int primary key,
-	tca_desc varchar (100),
+	tca_desc varchar (255),
 );
 
 create table pico_y_pala.cancelacion
 (
 	can_id int primary key,
 	can_tca_id int foreign key references pico_y_pala.tipo_cancelacion (tca_id),
-	can_motivo varchar (100)
+	can_motivo varchar (255)
 );
 
 create table pico_y_pala.compra
@@ -177,10 +179,10 @@ create table pico_y_pala.compra
 create table pico_y_pala.bono
 (
 	bon_id int primary key,
-	bon_afiliado_compra varchar (50) foreign key references pico_y_pala.afiliado (afi_username),
+	bon_afiliado_compra varchar (100) foreign key references pico_y_pala.afiliado (afi_username),
 	bon_pla_id int foreign key references pico_y_pala.planes (pla_id),
-	bon_nro_consultas_afiliado int,
-	bon_afiliado_utilizo varchar (50) foreign key references pico_y_pala.afiliado (afi_username),
+	bon_nro_consultas_afiliado numeric (18,0),
+	bon_afiliado_utilizo varchar (100) foreign key references pico_y_pala.afiliado (afi_username),
 	bon_fecha_utilizo datetime
 );
 
@@ -194,9 +196,9 @@ create table pico_y_pala.bono_compra
 
 create table pico_y_pala.turno
 (
-	tur_id int primary key,
-	tur_afi_username varchar (50) foreign key references pico_y_pala.afiliado (afi_username),
-	tur_pro_username varchar (50) foreign key references pico_y_pala.profesional (pro_username),
+	tur_id numeric (18,0) primary key,
+	tur_afi_username varchar (100) foreign key references pico_y_pala.afiliado (afi_username),
+	tur_pro_username varchar (100) foreign key references pico_y_pala.profesional (pro_username),
 	tur_fecha_hora datetime,
 	tur_can_id int foreign key references pico_y_pala.cancelacion (can_id)
 );
@@ -204,7 +206,7 @@ create table pico_y_pala.turno
 create table pico_y_pala.consulta
 (
 		con_id int primary key,
-		con_tur_id int foreign key references pico_y_pala.turno (tur_id),
+		con_tur_id numeric (18,0) foreign key references pico_y_pala.turno (tur_id),
 		con_bono_utilizado int foreign key references pico_y_pala.bono (bon_id),
 		con_fecha_llegada datetime,
 		con_fecha_consulta datetime,
