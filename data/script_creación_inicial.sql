8b1564d2c09194a38a1ca471718bdae0856c6d81
gstella <gstella@users.noreply.github.com>
2016-10-18 18:26:13 -0300

    Update script_creación_inicial.sql
    agrego carga de:
    especialidad
    tipo_especialidad
    usuario (en desarrollo)

--- data/script_creación_inicial.sql
+++ data/script_creación_inicial.sql
@@ -213,3 +213,75 @@ create table pico_y_pala.consulta
 		con_sintoma varchar (255),
 		con_enfermedad varchar (255)
 );
+
+-- TIPO_ESPECIALIDAD
+insert into pico_y_pala.tipo_especialidad
+		(tes_id,tes_desc)
+select distinct
+	Tipo_Especialidad_Codigo,
+	Tipo_Especialidad_Descripcion
+from gd_esquema.Maestra
+	where Tipo_Especialidad_Codigo is not null;
+
+--ESPECIALIDAD
+insert into pico_y_pala.especialidad
+		(
+		esp_id,
+		esp_desc,
+		esp_tes_id
+		)
+select distinct
+	Especialidad_Codigo,
+	Especialidad_Descripcion,
+	Tipo_Especialidad_Codigo	
+from gd_esquema.Maestra
+	where Especialidad_Codigo is not null;
+
+
+--USUARIO
+--sin terminar
+	
+insert into pico_y_pala.usuario
+	(
+	usu_username,
+	usu_apellido,
+	usu_nombre,
+	usu_nro_doc,
+	usu_tipo_doc,
+	usu_direccion,
+	usu_telefono,
+	usu_mail,
+	usu_fecha_nac,
+	usu_sexo,
+	usu_habilitado
+	)
+select distinct
+	paciente_apellido+'_'+replace (paciente_nombre,'   ','_') username,
+	Paciente_Apellido APELLIDO,
+	Paciente_Nombre NOMBRE,
+	Paciente_Dni DNI,
+	'DNI' TIPO_DOC,
+	Paciente_Direccion DIREECION,
+	Paciente_Telefono TELEFONO,
+	Paciente_Mail MAIL,
+	Paciente_Fecha_Nac FECHA_NAC,
+	'M' SEXO,
+	1 HABILITADO
+from gd_esquema.Maestra
+where paciente_apellido is not null
+union 
+select distinct	
+	Medico_Apellido+'_'+replace (Medico_Nombre,'   ','_'),
+	Medico_Apellido,
+	medico_nombre,
+	medico_dni,
+	'DNI' TIPO_DOC,
+	Medico_Direccion,
+	Medico_Telefono,
+	Medico_Mail,
+	Medico_Fecha_Nac,
+	'F',
+	1
+from gd_esquema.Maestra
+	where medico_apellido is not null
+
